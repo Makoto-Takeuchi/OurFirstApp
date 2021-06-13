2049f9c3305e2c5c714cf010b7bf8ccd29e8a989
KentaroYumita <m5251202@u-aizu.ac.jp>
2021-06-13 11:50:30 +0900

    Modified files related to database(It can execute)

--- OurQuarkus/our_quarkus/src/main/java/com/example/Sample.java
+++ OurQuarkus/our_quarkus/src/main/java/com/example/Sample.java
@@ -1,17 +1,20 @@
 package com.example;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
-//import java.time.LocalDateTime;
+import java.time.LocalDateTime;
 
 @Entity
-public class Sample /*extends PanacheEntity*/{
+public class Sample extends PanacheEntity{
 
-    private int id;
+    private Long id;
     private String name;
     private String description;
-//    private LocalDateTime created_date;
-//    private LocalDateTime updated_date;
+    private LocalDateTime created_date;
+    private LocalDateTime updated_date;
 
     //public String toString(){ return "ID: "+id+"\nname: "+name; }
 }
